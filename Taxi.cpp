ca3edbf1acd7a80064744c7ac6f5c8fbde096d7f
WillOWisp <P.U.L_S.A.R@mail.ru>
2021-10-17 23:17:29 +0500

    Добавил защиту от некорректных данных и комментарии.

--- Taxi.cpp
+++ Taxi.cpp
@@ -1,7 +1,8 @@
-﻿#include "iostream"
+﻿#include <iostream>
+#include <string>
 using namespace std;
 
-void qsort(int* mas, int* mas_index, int size) {
+void qsort(int* mas, int* mas_index, int size) { // быстрая сортировка
 	int i = 0;
 	int j = size - 1;
 	int mid = mas[size / 2];
@@ -27,43 +28,86 @@ void qsort(int* mas, int* mas_index, int size) {
 	}
 }
 
-
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-
-	int N;
+	int N, j;
+	string s;
+	bool f = 1;
 	cout << "Введите количество сотрудников фирмы (целое число 1<= N <=1000): ";
-	cin >> N;
-	int* mas1 = new int[N];
-	int* mas2 = new int[N];
-	int* mas1_index = new int[N];
-	int* mas2_index = new int[N];
-	cout << "\nВведите расстояния от работы до домов всех сотрудников (N положительных целых чисел, не превышающих 1000): ";
-	for (int i = 0; i < N; i++) {
-		cin >> mas1[i];
-		mas1_index[i] = i;
-		mas2_index[i] = i;
+	getline(cin, s);
+	for (int i = 0; i < s.size(); i++) { // посимвольная проверка
+		if ((s[i] < 48) || (s[i] > 57)) {
+			f = 0;
+			break;
+		}
 	}
-	cout << "\nВведите тарифы за проезд одного километра каждого такси (N положительных целых чисел, не превышающих 1000): ";
-	for (int i = 0; i < N; i++) {
-		cin >> mas2[i];
-	}
+	if (f) { // если всё нормально
+		N = stoi(s); // строка в число
+		int* mas1 = new int[N];
+		int* mas2 = new int[N];
+		int* mas1_index = new int[N];
+		int* mas2_index = new int[N];
+		for (int i = 0; i < N; i++) { // заполняю массив индексов 0, 1, 2, ..., N
+			mas1_index[i] = i;
+			mas2_index[i] = i;
+		}
 
-	qsort(mas1, mas1_index, N);
-	qsort(mas2, mas2_index, N);
-	for (int i = 0; i < N / 2; i++) {
-		swap(mas2[i], mas2[N - i - 1]);
-		swap(mas2_index[i], mas2_index[N - i - 1]);
-	}
-	qsort(mas1_index, mas2_index, N);
+		cout << "\nВведите расстояния от работы до домов всех сотрудников (N положительных целых чисел, не превышающих 1000, через enter без лишних символов): ";
+		bool f1 = 1;
+		for (int i = 0; i < N; i++) { // заполняю массив сотрудников
+			getline(cin, s);
+			for (int i = 0; i < s.size(); i++) { // проверка на вшивость очередного элемента
+				if ((s[i] < 48) || (s[i] > 57)) {
+					f1 = 0;
+				}
+			}
+			if (f1) { // если всё праивльно
+				mas1[i] = stoi(s); // строка в число
+			} else {
+				cout << "Некорректные данные.\n\n";
+				break;
+			}
+		}
+		if (f1) { // если данные были верны
+			cout << "\nВведите тарифы за проезд одного километра каждого такси (N положительных целых чисел, не превышающих 1000, через enter без лишних символов): ";
+			bool f2 = 1;
+			for (int i = 0; i < N; i++) { // всё по аналогии 
+				getline(cin, s);
+				f2 = 1;
+				for (int i = 0; i < s.size(); i++) {
+					if ((s[i] < 48) || (s[i] > 57)) {
+						f2 = 0;
+					}
+				}
+				if (f2) {
+					mas2[i] = stoi(s);
+				}
+				else {
+					cout << "Некорректные данные.\n\n";
+					break;
+				}
+			}
+			if (f2) { // если всё правильно
+				qsort(mas1, mas1_index, N); // сортировка массива сотрудников и их индексов
+				qsort(mas2, mas2_index, N); // сортировка массива такси и их индексов
+				for (int i = 0; i < N / 2; i++) { // реверс массива такси и их индексов (не писать же новую сортировку)
+					swap(mas2[i], mas2[N - i - 1]);
+					swap(mas2_index[i], mas2_index[N - i - 1]);
+				}
+				qsort(mas1_index, mas2_index, N); // сортировка индексов сотрудников и индексов такси чтобы вывести их по порядку
 
-	for (int i = 0; i < N; i++) {
-		cout << "\nСотрудник №" << i + 1 << " уедет на такси №" << mas2_index[i] + 1 << ";";
-	}
+				for (int i = 0; i < N; i++) { // вывод
+					cout << "\nСотрудник №" << i + 1 << " уедет на такси №" << mas2_index[i] + 1 << ";";
+				}
 
-	cout << "\n\n";
-	delete[] mas1, mas1_index, mas2, mas2_index;
+				cout << "\n\n";
+				delete[] mas1, mas1_index, mas2, mas2_index; // чистка
+			}
+		}
+	} else {
+		cout << "Некорректные данные.\n\n";
+	}
 	return 0;
 }
 
